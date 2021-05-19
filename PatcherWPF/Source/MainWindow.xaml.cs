71970e9ee061cd15348407096e1bf9fe09121aa6
Atlas Atlas <ifxuntogrags@gmail.com>
2021-05-19 13:29:19 +0200

    Réglage des problèmes de multi-window, déplacement + réunification en une seul fenêtre

--- PatcherWPF/Source/MainWindow.xaml.cs
+++ PatcherWPF/Source/MainWindow.xaml.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
 namespace PatcherWPF
@@ -23,29 +26,59 @@ namespace PatcherWPF
         private static int NBR_FILE_TO_DLL = 0;
         private static int NBR_ATM = 1;
 
-        public static Point coord;
         private static bool isShowed = false;
         bool _shown;
 
-        Source.Window1 form = new Source.Window1();
+        private static string NEUZ_RESOLUTION;
+        private static bool NEUZ_FULLSCREEN;
+        private static string NEUZ_SHADOW;
+        private static string NEUZ_SIGHT;
+        private static string NEUZ_TEXTURE;
+        private static string NEUZ_DETAILS;
+
+        private static string NEUZ_N_RESOLUTION = "";
+        private static bool NEUZ_N_FULLSCREEN = NEUZ_FULLSCREEN;
+        private static string NEUZ_N_SHADOW = "";
+        private static string NEUZ_N_SIGHT = "";
+        private static string NEUZ_N_TEXTURE = "";
+        private static string NEUZ_N_DETAILS = "";
+
 
         public MainWindow()
         {
             InitializeComponent();
             startBtn.IsEnabled = false;
-            LocationChanged += new EventHandler(Window_LocationChanged);
         }
 
-        private void Window_LocationChanged(object sender, EventArgs e)
+        private void window_Loaded(object sender, EventArgs e)
         {
-            form.Top = this.Top + this.Height;
-            form.Left = this.Left;
-            coord = new Point(this.Left, this.Top);
+            string[] neuz_ini = File.ReadAllLines("./neuz.ini");
+
+            string[] temp_res = neuz_ini[3].Split(' ');
+            string[] temp_fullscreen = neuz_ini[4].Split(' ');
+            string[] temp_shadow = neuz_ini[9].Split(' ');
+            string[] temp_sight = neuz_ini[6].Split(' ');
+            string[] temp_texture = neuz_ini[5].Split(' ');
+            string[] temp_detail = neuz_ini[7].Split(' ');
+
+            NEUZ_RESOLUTION = temp_res[1] + "x" + temp_res[2];
+            if (temp_fullscreen[1] == "0")
+            {
+                NEUZ_FULLSCREEN = false;
+            }
+            else
+            {
+                NEUZ_FULLSCREEN = true;
+            }
+            NEUZ_SHADOW = temp_shadow[1];
+            NEUZ_SIGHT = temp_sight[1];
+            NEUZ_TEXTURE = temp_texture[1];
+            NEUZ_DETAILS = temp_detail[1];
         }
 
         private void quitOnClick(object sender, RoutedEventArgs e)
         {
-            form.saveOptions();
+            saveOptions();
             Application.Current.Shutdown();
         }
 
@@ -55,10 +88,23 @@ namespace PatcherWPF
             if (_shown) return;
             _shown = true;
 
-            form.Show();
-            form.Visibility = Visibility.Hidden;
+            //Chargement options
+            var comboBoxItem = choixResolution.Items.OfType<ComboBoxItem>().FirstOrDefault(x => x.Content.ToString() == NEUZ_RESOLUTION);
+            int choix = choixResolution.SelectedIndex = choixResolution.Items.IndexOf(comboBoxItem);
+            choixResolution.SelectedIndex = choix;
+            if (NEUZ_FULLSCREEN)
+            {
+                fullscreen.IsChecked = true;
+            }
+            int.TryParse(NEUZ_SIGHT, out int tempsight);
+            sightBar.Value = tempsight;
+            int.TryParse(NEUZ_SHADOW, out int tempshadow);
+            shadowBar.Value = tempshadow;
+            int.TryParse(NEUZ_DETAILS, out int tempdetail);
+            detailBar.Value = tempdetail;
+            int.TryParse(NEUZ_TEXTURE, out int temptexture);
+            textBar.Value = temptexture;
 
-            coord = new Point(this.Left, this.Top);
             //PATCH
             File.Delete(@"./patchlog.txt");
             WriteLog("Connexion au serveur...\n");
@@ -224,7 +270,7 @@ namespace PatcherWPF
 
         private void startBtn_Click(object sender, RoutedEventArgs e)
         {
-            form.saveOptions();
+            saveOptions();
 
             //On lance le jeu
             Process.Start("./Neuz.exe", "62.210.104.245 sunkist");
@@ -259,26 +305,108 @@ namespace PatcherWPF
 
         private void optionsBtn_Click(object sender, RoutedEventArgs e)
         {
+            var converter = new System.Windows.Media.BrushConverter();
             if (isShowed)
             {
+                CanvasOptions.Opacity = 0;
                 isShowed = false;
-                form.Visibility = Visibility.Hidden;
             }
             else
             {
+                CanvasOptions.Opacity = 1;
                 isShowed = true;
-                form.Visibility = Visibility.Visible;
             }
+        }
+
+        public void saveOptions()
+        {
+            string[] neuz = File.ReadAllLines("./neuz.ini");
+            if (NEUZ_N_RESOLUTION != "")
+            {
+                string[] temp = NEUZ_N_RESOLUTION.Split('x');
+                neuz[3] = "resolution " + temp[0] + " " + temp[1];
+            }
+            if (NEUZ_N_FULLSCREEN)
+            {
+                neuz[4] = "fullscreen 1";
+            }
+            else
+            {
+                neuz[4] = "fullscreen 0";
+            }
+            if (NEUZ_N_SIGHT != "")
+            {
+                neuz[6] = "view " + NEUZ_N_SIGHT;
+            }
+            if (NEUZ_N_TEXTURE != "")
+            {
+                neuz[5] = "texture " + NEUZ_N_TEXTURE;
+            }
+            if (NEUZ_N_SHADOW != "")
+            {
+                neuz[9] = "shadow " + NEUZ_N_SHADOW;
+            }
+            if (NEUZ_N_DETAILS != "")
+            {
+                neuz[7] = "detail " + NEUZ_N_DETAILS;
+            }
+            File.WriteAllLines("./neuz.ini", neuz);
         }
 
         private void test_Click(object send, RoutedEventArgs e)
         {
-            testBtn.Opacity = 1;
+            //Fonction click (ouvrir Discord par exemple)
+            var psi = new ProcessStartInfo
+            {
+                FileName = DISCORD_LINK,
+                UseShellExecute = true
+            };
+            Process.Start(psi);
         }
 
         private void test_Pressed(object send, RoutedEventArgs e)
         {
             testBtn.Opacity = 0.5;
+        }
+
+        private void test_Release(object send, RoutedEventArgs e)
+        {
+            testBtn.Opacity = 1;
+        }
+
+        private void choixResolution_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBoxItem typeItem = (ComboBoxItem)choixResolution.SelectedItem;
+            NEUZ_N_RESOLUTION = typeItem.Content.ToString();
+        }
+
+        private void fullscreen_Checked(object sender, RoutedEventArgs e)
+        {
+            NEUZ_N_FULLSCREEN = (bool)fullscreen.IsChecked;
+        }
+
+        private void sightBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            int val = (int)sightBar.Value;
+            NEUZ_N_SIGHT = val.ToString();
+        }
+
+        private void textBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            int val = (int)textBar.Value;
+            NEUZ_N_TEXTURE = val.ToString();
+        }
+
+        private void detailBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            int val = (int)detailBar.Value;
+            NEUZ_N_DETAILS = val.ToString();
+        }
+
+        private void shadowBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            int val = (int)shadowBar.Value;
+            NEUZ_N_SHADOW = val.ToString();
         }
     }
 }
